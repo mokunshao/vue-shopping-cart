a217831e71733fdbbf9761e6d5cca4f10635a9af
mokunshao <1627510957@qq.com>
2018-12-21 22:06:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
 new Vue({
   el: "#app",
   data: {
-    productList: []
+    productList: [],
+    isCheckAll: false,
+    totalPrice: 0
+  },
+  filters: {
+    currency(value) {
+      return "￥" + value.toFixed(2);
+    }
   },
   mounted() {
-    this.getData();
+    this.$nextTick(() => {
+      this.getData();
+    });
   },
   methods: {
     getData() {
       axios.get("./data/cart.json").then(response => {
         this.productList = response.data.result.list;
       });
+    },
+    selectProduct(item) {
+      if (typeof item.isChecked === "undefined") {
+        this.$set(item, "isChecked", true);
+      } else {
+        item.isChecked = !item.isChecked;
+      }
+      this.calcTotalPrice();
+    },
+    CheckAll() {
+      this.isCheckAll = !this.isCheckAll;
+      this.productList.forEach(item => {
+        if (typeof item.isChecked === "undefined") {
+          this.$set(item, "isChecked", true);
+        } else {
+          item.isChecked = this.isCheckAll;
+        }
+      });
+      this.calcTotalPrice();
+    },
+    calcTotalPrice() {
+      this.totalPrice = 0;
+      this.productList.forEach(item => {
+        if (item.isChecked) {
+          this.totalPrice += item.price * item.amount;
+        }
+      });
+    },
+    minus(item) {
+      item.amount > 1 ? item.amount-- : 1;
+      this.calcTotalPrice();
+    },
+    add(item) {
+      item.amount++;
+      this.calcTotalPrice();
     }
   }
 });
